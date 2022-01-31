5104a3d0a1c4328b9aa2c34cc4b14076a2248f20
s_umar <sadekov.u.r@gmail.com>
2022-01-31 10:59:50 +0300

    init

---
+++ SlyApp.js
@@ -0,0 +1,86 @@
+Vue.component('sly-app', {
+  props: ['url', 'initState'],
+  template: `
+<div>
+  <slot :state="state" :data="data" :command="command" :http="http" />
+</div>
+  `,
+
+  data: function () {
+    return {
+      task: {},
+      state: {},
+      data: {},
+      context: {},
+      ws: null,
+    };
+  },
+
+  computed: {
+    formattedUrl () {
+      return this.url.replace(/\/$/, '');
+    },
+  },
+
+  methods: {
+    command(command) {
+      console.log('Command!', command);
+      this.ws.send(JSON.stringify({ command: command, state: this.state }));
+    },
+
+    http(command) {
+      console.log('Http!', command);
+
+      fetch(`${this.formattedUrl}${command}`, {
+          method: 'POST', body: JSON.stringify(this.state), headers: {'Content-Type': 'application/json'}
+      }).then(res => res.json()).then((json) => {
+          this.merge(json);
+      });
+    },
+
+    async getJson(path) {
+      return fetch(`${this.formattedUrl}${path}`, {
+        method: 'POST',
+      })
+      .then(res => res.json()).then((json) => {
+        return json;
+      });
+    },
+
+    merge(payload) {
+      if (payload.state) {
+        Object.assign(this.state, payload.state);
+      }
+
+      if (payload.data) {
+        Object.assign(this.state, payload.data);
+      }
+    },
+  },
+
+  created() {
+    console.log('First Init WS');
+    this.state = await this.getJson('/sly-app-state');
+    this.data = await this.getJson('/sly-app-data');
+
+    this.ws = new WebSocket(`ws${document.location.protocol === "https:" ? "s" : ""}://${this.url.replace("http://", "").replace("https://", "").replace(/\/$/, '')}/ws`);
+    this.ws.onmessage = (event) => {
+      console.log('Message received from Python', event.data);
+      this.merge(event.data);
+    };
+  },
+});
+
+window.slyApp = {
+  app: null,
+  init() {
+    this.app = new Vue({
+      el: '#sly-app',
+      computed: {
+          document() {
+              return document;
+          }
+      },
+    });
+  },
+};
